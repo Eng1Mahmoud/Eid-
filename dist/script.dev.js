6943545489b115bebf5e6098b7a67dabed4575b6
Eng Mahmoud <Eng1Mahmoud@gitHub.com>
2022-04-30 23:27:29 +0200

    Finsh

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -4,12 +4,20 @@ var left = document.querySelector(".contener .content .left");
 var right = document.querySelector(".contener .content .right");
 var open_sound = new Audio("./mixkit-scary-wooden-door-opening-190.wav");
 
-window.onload = function () {
-  left.parentElement.onclick = function () {
+left.parentElement.onclick = function () {
+  if (left.classList.contains("openl") && right.classList.contains("openr")) {
+    left.classList.add("closel");
+    right.classList.add("closer");
+    left.classList.remove("openl");
+    right.classList.remove("openr");
+  } else {
+    left.classList.remove("closel");
+    right.classList.remove("closer");
     left.classList.add("openl");
     right.classList.add("openr");
-    setTimeout(function () {
-      open_sound.play();
-    }, 1000);
-  };
+  }
+
+  setTimeout(function () {
+    open_sound.play();
+  }, 500);
 };
--- script.js
+++ script.js
@@ -2,16 +2,30 @@
 let right = document.querySelector(".contener .content .right");
 let open_sound = new Audio("./mixkit-scary-wooden-door-opening-190.wav");
 
-window.onload = () => {
+
     
    
         left.parentElement.onclick = ( ) => {
+          if(left.classList.contains("openl") && right.classList.contains("openr")){
             
+            left.classList.add("closel");
+            right.classList.add("closer");
+              
+            left.classList.remove("openl");
+            right.classList.remove("openr");
+            
+          }
+          else{
+            left.classList.remove("closel");
+            right.classList.remove("closer");
             left.classList.add("openl");
             right.classList.add("openr");
+            
+          }
+            
+          
             setTimeout( () => {
               open_sound.play()
-            },1000);
+            },500)
             };
     
-}; 
